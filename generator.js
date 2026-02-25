53c8343edcf39bd2fbc70f6a48d944f7f425596f
dvsqwx <dvsqwx@proton.me>
2026-02-25 23:57:30 +0200

    Add random number generator function
    add a generator function

--- generator.js
+++ generator.js
@@ -1,1 +1,10 @@
 "use strict"
+
+/random numbers generator
+
+function* randomNumberGenerator() {
+    while (true) {
+        let num = Math.floor(Math.random() * 100) + 1;
+        yield num;
+    }
+}
