7d01713539466e8104fc3946c1184310917d299e
dvsqwx <dvsqwx@proton.me>
2026-02-27 18:26:58 +0200

    Implement startProcessing function with interval

--- processor.js
+++ processor.js
@@ -1,3 +1,12 @@
 "use strict"
 
-function startProcessing {}
+function startProcessing(iterator, seconds) {
+    let total = 0;
+    let count = 0;
+
+    let intervalId = setInterval(() => {
+        let nextValue = iterator.next().value;
+        total += nextValue;
+        count++;
+        console.log(`num: ${nextValue} | total: ${total}`);
+    }, 500);
